94a9ec93ad81ee5ac71133e90aaa6359a1a5efb3
Poltree <76885489+Poltree@users.noreply.github.com>
2022-04-24 06:49:08 -0400

    Add files via upload

---
+++ Headset.js
@@ -0,0 +1,42 @@
+res_array = []
+picture=""
+fin_status="" 
+function setup(){
+canvas = createCanvas(700,450)
+canvas.center()
+cocossd_var = ml5.objectDetector('cocossd',modelloaded)
+}
+function modelloaded(){
+    console.log("THE MODEL IS LOADED")
+    fin_status = true
+    cocossd_var.detect(picture,gotResults)
+}
+function gotResults(error,results){
+if (error){
+    console.log("error")
+
+}
+else{
+    console.log(results)
+    res_array = results;
+}
+}
+
+function draw(){
+image(picture,0,0,700,450)
+if(status != ""){
+for(var v=0; v < res_array.length; v++){
+fill("#20C20E")
+text(res_array[v].label,res_array[v].x + 10,res_array[v].y + 15)
+noFill()
+stroke("#20C20E")
+
+rect(res_array[v].x,res_array[v].y,res_array[v].width,res_array[v].height)
+}
+}
+
+}
+function preload(){
+
+picture = loadImage("headset.jpg")
+}
---
+++ Led.js
@@ -0,0 +1,49 @@
+res_array = []
+picture=""
+status="" 
+perc = ""
+function setup(){
+canvas = createCanvas(700,450)
+canvas.center()
+cocossd_var = ml5.objectDetector('cocossd',modelloaded)
+document.getElementById("status").innerHTML = "Detecting Objects"
+}
+function modelloaded(){
+    console.log("THE MODEL IS LOADED")
+    status = true
+    cocossd_var.detect(picture,gotResults)
+}
+function gotResults(error,results){
+if (error){
+    console.log("error")
+
+}
+else{
+    console.log(results)
+    res_array = results;
+}
+}
+
+function draw(){
+image(picture,0,0,700,450)
+if(status != ""){
+    r = random(255)
+    g = random(255)
+    b = random(255)
+for(var v=0; v < res_array.length; v++){
+fill(r,g,b)
+perc= floor(res_array[v].confidence*100)
+text(res_array[v].label + " " + perc + "%" + " ", res_array[v].x + 10,res_array[v].y + 15)
+noFill()
+stroke(r,b,g)
+
+rect(res_array[v].x,res_array[v].y,res_array[v].width,res_array[v].height)
+document.getElementById("status").innerHTML = "Detected Objects"
+}
+}
+
+}
+function preload(){
+
+picture = loadImage("Led.jpg")
+}
